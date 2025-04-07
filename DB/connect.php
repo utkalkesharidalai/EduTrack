2c373983ede36dcfd1963f0a686d6bd9d2fe12f6
utkalkesharidalai <utkalkeshridalai@gmail.com>
2025-04-07 16:27:05 +0530

    cc

--- DB/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "193.203.184.156";
-$username = "u807244070_edu	"; 
-$password = "Edueduedu@123"; 
-$dbname = "u807244070_edu";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-?>
