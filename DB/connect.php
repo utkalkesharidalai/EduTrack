1eb502e4e7dc6003389c13b1b89cbc2fcf038deb
utkalkesharidalai <utkalkeshridalai@gmail.com>
2025-04-07 15:36:58 +0530

    Update connect.php

--- DB/connect.php
+++ DB/connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root"; 
-$password = ""; 
-$dbname = "gp_it_db";
+$servername = "193.203.184.156";
+$username = "u807244070_edu	"; 
+$password = "Edueduedu@123"; 
+$dbname = "u807244070_edu";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
