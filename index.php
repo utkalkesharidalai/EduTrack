9820d9d4d4baabea44f7e8f26ead42cecaf23e60
utkalkesharidalai <utkalkeshridalai@gmail.com>
2025-04-08 19:33:56 +0530

    tlogin path given

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
                         <ul class="dropdown-menu dropdown-menu-end">
                             <li><a class="dropdown-item" href="..//partials/login/adminlogin.php"><i class="fas fa-user-shield me-2"></i>Admin</a></li>
                             <li><a class="dropdown-item" href="login.php"><i class="fas fa-user-graduate me-2"></i>Student</a></li>
-                            <li><a class="dropdown-item" href="html/tlogin.php"><i class="fas fa-chalkboard-teacher me-2"></i>Teacher</a></li>
+                            <li><a class="dropdown-item" href="..//partials/login/tlogin.php"><i class="fas fa-chalkboard-teacher me-2"></i>Teacher</a></li>
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
