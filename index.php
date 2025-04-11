5a847c72f3b1408a979c2bc2fb93f5871422790f
sharmistha jena <sharmisthajena972@gmai.com>
2025-04-11 17:30:33 +0530

    .

--- index.php
+++ index.php
@@ -45,7 +45,43 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark  sticky-top shadow-sm" style="background-color: #0c111a;">
         <div class="container">
-
+  
+        <a class="navbar-brand fw-bold" href="index.html">
+                <img src="partials/images/h_logo.png" alt="EduTrack Logo" height="40" class="me-2">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mainNavbar">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../about.php">About Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../gallery.php">Gallery</a></li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown me-2">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                            <i class="fas fa-sign-in-alt me-1"></i> Login
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="partials/login/adminlogin.php"><i class="fas fa-user-shield me-2"></i>Admin</a></li>
+                            <li><a class="dropdown-item" href="partials/login/login.php"><i class="fas fa-user-graduate me-2"></i>Student</a></li>
+                            <li><a class="dropdown-item" href="partials/login/tlogin.php"><i class="fas fa-chalkboard-teacher me-2"></i>Teacher</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                            <i class="fas fa-user-plus me-1"></i> Register
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="partials/register/sregister.php"><i class="fas fa-user-graduate me-2"></i>Student</a></li>
+                            <li><a class="dropdown-item" href="partials/register/tregister.php"><i class="fas fa-chalkboard-teacher me-2"></i>Teacher</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
     <!-- Hero Section -->
     <section class="py-5 bg-light position-relative overflow-hidden">
